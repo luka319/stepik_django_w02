5c5b75d26ebbe08efafe1133c36b8ac7fb93a46a
Luka Pacholli <luka319@i.ua>
2021-03-21 00:41:51 +0200

    3 tours_view

--- tours/views.py
+++ tours/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 # Create your views here.
 import random
-from tours.data import title, subtitle, description, tours
+from tours.data import title, subtitle, description, departures, tours
 
 def main_view(request):
     random_tours = dict(random.sample(tours.items(), 6))
@@ -19,8 +19,47 @@ def departure_view(request, departure):
 
 
 def tour_view(request, id_):
+    # tours = data.tours
+    # departures = data.departures
 
-    return render(request, "tours/tour.html", )
+    for key, val01 in tours.items():
+        if key == int(id_):
+             for ke, va in val01.items():
+                  if ke == "title":
+                     title = va
+                  if ke == "stars":
+                     stars = "★" * int(va)
+                  if ke == "country":
+                     country = va
+                  if ke == "price":
+                     price = va
+                  if ke == "picture":
+                      picture = va
+                  if ke == "nights":
+                      nights = va
+                  if ke == "description":
+                      description = va
+                  if ke == "departure":
+                      departure2 = va
+                      departure1 = departures[va]
+
+    return render(request, "tours/tour.html", context={
+        'title': title,
+        'tours': tours,
+        'stars': stars,
+        'price': price, 'country':country, 'picture': picture,
+        'nights': nights, 'departure2': departure2,
+        'departure1':departure1,
+        'description': description, 'departures': departures
+    })
+
+    # return render_template("tour.html",tours=tours, title=title, stars=stars,
+    #         price=price, country=country,picture=picture,nights=nights, departure=departure,
+    #         description=description,
+    #         departures=departures)
+
+
+
 
 from django.http import HttpResponse, HttpResponseNotFound
 
