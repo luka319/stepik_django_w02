3c6bc7474ddee35200828e2cd0dfeade6458d04c
Luka Pacholli <luka319@i.ua>
2021-03-20 23:39:11 +0200

    2 main_view

--- tours/views.py
+++ tours/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render
-
 # Create your views here.
+import random
+from tours.data import title, subtitle, description, tours
 
 def main_view(request):
-
-    return render(request, "index.html", )
+    random_tours = dict(random.sample(tours.items(), 6))
+    return render(request, "tours/index.html", context={
+        'title':title,
+        'subtitle':subtitle,
+        'description':description,
+        'tours': random_tours,
+    } )
 
 
 def departure_view(request, departure):
 
-    return render(request, "departure.html", )
+    return render(request, "tours/departure.html", )
 
 
 def tour_view(request, id_):
 
-    return render(request, "tour.html", )
+    return render(request, "tours/tour.html", )
+
+from django.http import HttpResponse, HttpResponseNotFound
+
+def custom_handler404(request, exception):
+    return HttpResponseNotFound('Ой, что то сломалось... Простите извините!')
